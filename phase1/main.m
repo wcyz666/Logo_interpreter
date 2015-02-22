04ac7f2eccc79b6d8d13965448ce5740ffa6ee19
wcyz666 <wcyz666@wcyz666deMac.local>
2015-02-21 19:20:23 -0800

    Initial Commit

---
+++ phase1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  phase1
+//
+//  Created by wcyz666 on 15-2-21.
+//  Copyright (c) 2015年 wcyz666. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
